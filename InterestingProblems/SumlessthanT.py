e099a752b707d23b1daf2770a4b63c669cb76d9b
Ankush Roy <ankush1123roy@gmail.com>
2013-10-20 18:15:35 -0600

    Target K

---
+++ InterestingProblems/SumlessthanT.py
@@ -0,0 +1,24 @@
+from copy import deepcopy
+def SumlessthanT(ar,K):
+    ar.sort()
+    I = 0 
+    L = len(ar) - 1
+    results = 0 
+    LNum = ar[0]
+    HNum = ar[-1]
+    if LNum + HNum < K:
+        results = 1
+    while I <=  L:
+        if ar[I] + ar[L]  < K and (LNum != ar[I] or HNum != ar[L]):
+            results += 1
+            LNum = ar[I] 
+            HNum = ar[L]
+            I += 1
+        else:
+            L -= 1
+    print results
+            
+m = input()
+K = input()
+m = [int(i) for i in raw_input().strip().split()]
+SumlessthanT(m,K)
